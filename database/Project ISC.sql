0309623dc49af39ce04fe62d229213906271a88e
vubinh132 <vuqbinh995@gmail.com>
2017-03-30 19:26:31 +0700

    them script cho active

--- database/Project ISC.sql
+++ database/Project ISC.sql
@@ -3,5 +3,6 @@ id int auto_increment primary key,
 intake_id varchar(30) not null unique,
 intake_name varchar(30) not null,
 start_date date,
-end_date date
+end_date date,
+active boolean not null
 )
