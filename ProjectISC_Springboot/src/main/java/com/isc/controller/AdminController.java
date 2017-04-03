1ea21fa328cc320b2b96785683721f2a949f7b75
vubinh132 <vuqbinh995@gmail.com>
2017-04-04 01:09:55 +0700

    table

---
+++ ProjectISC_Springboot/src/main/java/com/isc/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.isc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	@RequestMapping(value = "/admin/")
+	public String index() {
+		return "admin/index";
+	}
+	@RequestMapping(value = "/admin/intake")
+	public String intake() {
+		return "admin/intake";
+	}
+}
