a801d9b94dba152001280382469c60f5d4f0af13
VMT <vominhthu0508@gmail.com>
2017-04-05 19:48:39 +0700

    load dữ liệu, tìm kiếm theo tên, sắp xếp trong trang admin/intake

--- ProjectISC_Springboot/src/main/resources/static/admin/scripts/intakeCtrl.js
+++ ProjectISC_Springboot/src/main/resources/static/admin/scripts/intakeCtrl.js
@@ -1,5 +1,104 @@
-app.controller('intakeCtrl', function($scope, $http) {
-	$http.get("http://localhost:8080/api/school").then(function(response) {
-		$scope.list = response.data;
-	});
+app.controller('intakeCtrl', function($scope, $http,$filter) {
+	
+	//Lấy danh sách Intake
+	function GetListIntake(){
+    $http.get('http://localhost:8080/api/intake').
+        then(function(response) {
+        	$scope.list= response.data;
+        });
+	};
+	GetListIntake();
+	
+	$scope.sortType = 'intakeName';
+	$scope.filterTable = '';
+	//Tìm kiếm theo tên
+	$scope.filterSort = function(element) {
+		if ($filter('filter')([element], $scope.filterTable).length > 0) {
+			return 1;
+		}
+		return 2;
+	};
+	function matchFirstChar(c, string) {
+		return (string.charAt(0) == c);
+	}
+	
+	function removeFirstChar(string) {
+		return string.slice(1);
+	}
+	
+	function removeDash(label) {
+		if (matchFirstChar('-', label)) {
+			return removeFirstChar(label);
+		}
+		return label;
+	}
+	function addDash(label) {
+		if (!matchFirstChar('-', label)) {
+			return '-' + label;
+		}
+		return label;
+	}
+	
+	// formatting functions
+	// for displaying table headers and tooltips
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+	
+	function makeReadableLabel(label) {
+		var formatted = label;
+		switch (label) {
+			case 'firstName':
+				formatted = 'first name';
+				break;
+			case 'lastName':
+				formatted = 'last name';
+		}
+		return formatted;
+	}
+	
+	// sort functions
+	// add or remove '-' to sort up or down
+	$scope.sortReverse = function(set) {
+		set = set || false;
+		if (set || !matchFirstChar('-', $scope.sortType)) {
+			$scope.sortType = addDash($scope.sortType);
+		} else {
+			$scope.sortType = removeDash($scope.sortType);
+		}
+	};
+	
+	// sort a column with a single data attribute
+	$scope.singleSort = function(label) {
+		if ($scope.sortType == label) {
+			$scope.sortReverse();
+		} else {
+			$scope.sortType = label;
+		}
+	};
+	$scope.sortDescend = function(label1, label2) {
+		label2 = label2 || '';
+		return ($scope.sortType == label1 || $scope.sortType == label2);
+	};
+	
+	$scope.sortAscend = function(label1, label2) {
+		label2 = label2 || '';
+		return ($scope.sortType == ('-' + label1) || $scope.sortType == ('-' + label2));
+	};
+	
+	// show a tooltip displaying how a column is sorted
+	$scope.sortTooltip = function(label1, label2) {
+		label2 = label2 || '';
+		
+		var order = 'descending';
+		if (matchFirstChar('-', $scope.sortType)) {
+			order = 'ascending';
+		}
+		
+		var baseSortType = removeDash($scope.sortType);
+		if (label1 == baseSortType || label2 == baseSortType) {
+			return capitalizeFirstLetter((makeReadableLabel(baseSortType)) + ' ' + order);
+		}
+		return 'Sort by ' + makeReadableLabel(label1) + ' or ' + makeReadableLabel(label2);
+	};
 });
