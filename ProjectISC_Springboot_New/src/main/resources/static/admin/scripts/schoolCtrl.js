f26a04c2dc84dc8c177c8f83c663a3e4c4f682d8
SUONGSCOMPUTER\suong <suongpham53@gmail.com>
2017-04-07 09:05:26 +0700

    CRUD School

--- ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
+++ ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
@@ -1,5 +1,19 @@
-app.controller('intakeCtrl', function($scope, $http) {
-	$http.get("http://localhost:8080/api/school").then(function(response) {
-		$scope.list = response.data;
+
+app.controller('schoolCtrl', function($scope, $http) {
+
+	var url = "http://localhost:8080/api/school";
+	$http.get(url).then(function(response) {
+		$scope.list= response.data;
 	});
+	$scope.modal = function(id){
+		console.log(id);
+		$("#myModal").modal('show');
+	}
+	$scope.deleteSchool = function(id){
+		
+		$("#myModal_xoa").modal('show');
+	}
+
+
+
 });
