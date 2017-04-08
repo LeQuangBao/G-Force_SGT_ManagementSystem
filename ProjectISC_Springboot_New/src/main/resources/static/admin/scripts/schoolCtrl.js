e645b32b5ad01cadb5471cea1792df5645998759
Le Quang Bao <lequangbao13351@gmail.com>
2017-04-08 14:15:11 +0700

    CHANGED
    CHANGED

--- ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
+++ ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
@@ -1,4 +1,4 @@
-app.controller('intakeCtrl', function($scope, $http) {
+app.controller('schoolCtrl', function($scope, $http) {
 	$http.get("http://localhost:8080/api/school").then(function(response) {
 		$scope.list = response.data;
 	});
