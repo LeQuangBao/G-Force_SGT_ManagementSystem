958b7a1c6e33673efd1b080c8a9bf0f457363da6
Le Quang Bao <lequangbao13351@gmail.com>
2017-04-08 14:09:33 +0700

    Change UI
    CHANGE UI

--- ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
+++ ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/schoolCtrl.js
@@ -2,6 +2,5 @@ app.controller('intakeCtrl', function($scope, $http) {
 	$http.get("http://localhost:8080/api/school").then(function(response) {
 		$scope.list = response.data;
 	});
-	
+	$scope.filterTable = '';
 });
-$scope.filterTable = '';
