ed1bd357dcd0696bb5e60c44dd83ffc96597f478
Pham Thi Thu Suong <suongpham53@gmail.com>
2017-04-08 15:36:38 +0700

    add built folder

---
+++ ProjectISC_Springboot_New/target/classes/static/admin/scripts/schoolCtrl.js
@@ -0,0 +1,6 @@
+app.controller('schoolCtrl', function($scope, $http) {
+	$http.get("http://localhost:8080/api/school").then(function(response) {
+		$scope.list = response.data;
+	});
+	$scope.filterTable = '';
+});
