c1bca597499a0a3dd13cd9e52cafd4b34ad53877
jasminecrazy <suongpham53@gmail.com>
2017-05-05 22:45:10 +0700

    Revert "thông báo invalid pass or email"
    This reverts commit 66a4b24cdb7a71e4c6f36d6db5fa8f711a6b8956.

--- ProjectISC_Springboot_New/src/main/java/com/isc/SecurityConfiguration.java
+++ ProjectISC_Springboot_New/src/main/java/com/isc/SecurityConfiguration.java
@@ -12,9 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//annotation nay dung de xac dinh class nay de cau hinh
+
 @Configuration
-//anno nay kich hoat spring security
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
@@ -26,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//phan quyen request
+
 		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().antMatchers("/admin/**")
 				.access("hasRole('ROLE_ADMIN')").and().formLogin().loginPage("/login")
 				.successHandler(customSuccessHandler).usernameParameter("username").passwordParameter("password").and()
@@ -39,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-//PassEncoder lam nhiem vu ma hoa pass, to use it , it must be 'bean'
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
