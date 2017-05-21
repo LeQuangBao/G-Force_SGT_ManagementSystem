967dda8095158195c18fcebd258f3ffa42b49842
ThachLinh <nlinh6894@gmail.com>
2017-05-21 14:14:24 +0700

    apply new table for all table sub courses

--- ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/instructorCtrl.js
+++ ProjectISC_Springboot_New/src/main/resources/static/admin/scripts/instructorCtrl.js
@@ -23,9 +23,178 @@ app.controller('instructorCtrl', function($scope, $http,$filter) {
 	    	$http.get("api/instructor")
 	    .then(function(response) {
 			$scope.list=response.data;
+			$scope.gridOptions.data = response.data;
 	    });
 	    }
 	    getAllinstructors();
+	    // tạo dữ liệu cho table
+	    $scope.gridOptions = {
+	    		noUnselect : true,
+	    		multiSelect: false,
+	    		enableRowSelection: true,
+	    		enableRowHeaderSelection: false,
+	    	    enableSelectAll: false,
+	    	    enableGridMenu: true,
+	    		enableFiltering: true,
+	    		enableColumnResize: true,
+	    		enableColumnMenus: false,
+	    	    paginationPageSizes: [15, 30, 50, 100],
+	    	    paginationPageSize: 15,
+	    	    columnDefs: [
+	    		      { name: 'username',displayName:'User Name' },
+	    		      { name: 'lastname', displayName : 'Last Name' },
+	    		      { name: 'firstname', displayName : 'First Name'},
+	    		      { name: 'birthday', visible : false },
+	    		      { name: 'email', visible : false },
+	    		      { name: 'phone', visible : false },
+	    		      { name: 'address', visible : false },
+	    		      { name: 'status'},
+	    		      { name: 'Action',enableSorting: false,enableFiltering: false,
+	    		             cellTemplate:'<button ng-click="grid.appScope.viewInstructor(row.entity)" data-toggle="modal" class="btn btn-success btn-sm" data-tooltip ="tooltip" title="View detail informations" data-target="#myModal_detail"><span class="glyphicon glyphicon-eye-open"></span></button>' 
+	    		            	 			+'<button class="btn btn-primary btn-sm" ng-click="grid.appScope.getInstructor(row.entity)" data-tooltip ="tooltip" title="Edit"	data-toggle="modal" data-target="#myModal_sua"><span class="glyphicon glyphicon-edit"></span></button>'
+	    		            	 			+'<button ng-click="grid.appScope.getInstructor(row.entity)" data-toggle="modal" class="btn btn-danger btn-sm" data-tooltip ="tooltip" title="Delete" data-target="#myModal_xoa"><span class="glyphicon glyphicon-remove"></span></button>'
+	    		      }
+	    		    ]
+	    	  };
+	    // lọc dữ toàn bộ dữ liệu
+	    $scope.refreshData = function (termObj) {
+	        $scope.gridOptions.data = $scope.list;
+
+	        while (termObj) {
+	            var oSearchArray = termObj.split(' ');
+	            $scope.gridOptions.data = $filter('filter')($scope.gridOptions.data, oSearchArray[0], undefined);
+	            oSearchArray.shift();
+	            termObj = (oSearchArray.length !== 0) ? oSearchArray.join(' ') : '';
+	        }
+	    };
+	    
+	    $scope.sortType = 'firstName';
+	    $scope.filterTable = '';
+	    // filter list
+	    $scope.listfiltered = function(element) {
+	        return $filter('filter')(element, $scope.filterTable);
+	    };
+
+	    function matchFirstChar(c, string) {
+	        return (string.charAt(0) == c);
+	    }
+
+	    function removeFirstChar(string) {
+	        return string.slice(1);
+	    }
+
+	    function removeDash(label) {
+	        if (matchFirstChar('-', label)) {
+	            return removeFirstChar(label);
+	        }
+	        return label;
+	    }
+
+	    function addDash(label) {
+	        if (!matchFirstChar('-', label)) {
+	            return '-' + label;
+	        }
+	        return label;
+	    }
+
+	    // formatting functions
+	    // for displaying table headers and tooltips
+	    function capitalizeFirstLetter(string) {
+	        return string.charAt(0).toUpperCase() + string.slice(1);
+	    }
+
+	    function makeReadableLabel(label) {
+	        var formatted = label;
+	        switch (label) {
+	            case 'firstName':
+	                formatted = 'first name';
+	                break;
+	            case 'lastName':
+	                formatted = 'last name';
+	        }
+	        return formatted;
+	    }
+
+	    // sort functions
+	    // add or remove '-' to sort up or down
+	    $scope.sortReverse = function(set) {
+	        set = set || false;
+	        if (set || !matchFirstChar('-', $scope.sortType)) {
+	            $scope.sortType = addDash($scope.sortType);
+	        } else {
+	            $scope.sortType = removeDash($scope.sortType);
+	        }
+	    };
+
+	    // sort a column with a single data attribute
+	    $scope.singleSort = function(label) {
+	        if ($scope.sortType == label) {
+	            $scope.sortReverse();
+	        } else {
+	            $scope.sortType = label;
+	        }
+	    };
+	    $scope.sortDescend = function(label1, label2) {
+	        label2 = label2 || '';
+	        return ($scope.sortType == label1 || $scope.sortType == label2);
+	    };
+
+	    $scope.sortAscend = function(label1, label2) {
+	        label2 = label2 || '';
+	        return ($scope.sortType == ('-' + label1) || $scope.sortType == ('-' + label2));
+	    };
+
+	    // show a tooltip displaying how a column is sorted
+	    $scope.sortTooltip = function(label1) {
+
+	        var order = 'descending';
+	        if (matchFirstChar('-', $scope.sortType)) {
+	            order = 'ascending';
+	        }
+
+	        var baseSortType = removeDash($scope.sortType);
+	        if (label1 == baseSortType) {
+	            return capitalizeFirstLetter((makeReadableLabel(baseSortType)) + ' ' + order);
+	        }
+	        return 'Sort by ' + makeReadableLabel(label1)
+	    };
+	    // Phân trang
+	    $scope.currentPage = 1;
+	    // max size of the pagination bar
+	    $scope.maxPaginationSize = 10;
+	    // calculate total pages
+
+	    $scope.itemsPerPage = $scope.rowdata.selectedOption.id;
+	    $scope.updatePageIndexes = function() {
+	        var totalPages = Math.ceil($scope.list.length / $scope.maxPaginationSize);
+	        if (totalPages <= 10) {
+	            // less than 10 total pages so show all
+	            $scope.firstIndex = 1;
+	            $scope.lastIndex = totalPages;
+	        } else {
+	            // more than 10 total pages so calculate start and
+	            // end pages
+	            if ($scope.currentPage <= 6) {
+	                $scope.firstIndex = 1;
+	                $scope.lastIndex = 10;
+	            } else if ($scope.currentPage + 4 >= totalPages) {
+	                $scope.firstIndex = totalPages - 9;
+	                $scope.lastIndex = totalPages;
+	            } else {
+	                $scope.firstIndex = $scope.currentPage - 5;
+	                $scope.lastIndex = $scope.currentPage + 4;
+	            }
+	        }
+	        $scope.firstIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+	        $scope.lastIndex = $scope.currentPage * $scope.itemsPerPage;
+	    };
+	    $scope.updatePageIndexes();
+
+	    $scope.showList = function(index) {
+	        return ((index >= $scope.firstIndex) && (index < $scope.lastIndex));
+	    }
+	    
+	 
 	    $scope.sortType = 'firstName';
     	$scope.filterTable = '';
 	 // Tìm kiếm theo tên
@@ -52,38 +221,38 @@ app.controller('instructorCtrl', function($scope, $http,$filter) {
         }
         
     	$scope.prev_img='';
-    	// Phân trang
-    	$scope.currentPage = 1;
-    	// max size of the pagination bar
-    	$scope.maxPaginationSize = 10;
-    	$scope.itemsPerPage = 15;
-    	$scope.updatePageIndexes = function () {
-    		var totalPages = Math.ceil($scope.list.length / $scope.maxPaginationSize);
-    		if (totalPages <= 10) {
-                // less than 10 total pages so show all
-    			$scope.firstIndex = 1;
-    			$scope.lastIndex = totalPages;
-            } else {
-                // more than 10 total pages so calculate start and end pages
-                if ($scope.currentPage <= 6) {
-                	$scope.firstIndex = 1;
-                	$scope.lastIndex = 10;
-                } else if ($scope.currentPage + 4 >= totalPages) {
-                	$scope.firstIndex = totalPages - 9;
-                	$scope.lastIndex = totalPages;
-                } else {
-                	$scope.firstIndex = $scope.currentPage - 5;
-                	$scope.lastIndex = $scope.currentPage + 4;
-                }
-            }
-    		$scope.firstIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
-    		$scope.lastIndex = $scope.currentPage * $scope.itemsPerPage;
-    	};
-    	$scope.updatePageIndexes();
-    	
-    	$scope.showList=function(index){
-    		return ((index >= $scope.firstIndex) && (index < $scope.lastIndex));
-    	}
+//    	// Phân trang
+//    	$scope.currentPage = 1;
+//    	// max size of the pagination bar
+//    	$scope.maxPaginationSize = 10;
+//    	$scope.itemsPerPage = 15;
+//    	$scope.updatePageIndexes = function () {
+//    		var totalPages = Math.ceil($scope.list.length / $scope.maxPaginationSize);
+//    		if (totalPages <= 10) {
+//                // less than 10 total pages so show all
+//    			$scope.firstIndex = 1;
+//    			$scope.lastIndex = totalPages;
+//            } else {
+//                // more than 10 total pages so calculate start and end pages
+//                if ($scope.currentPage <= 6) {
+//                	$scope.firstIndex = 1;
+//                	$scope.lastIndex = 10;
+//                } else if ($scope.currentPage + 4 >= totalPages) {
+//                	$scope.firstIndex = totalPages - 9;
+//                	$scope.lastIndex = totalPages;
+//                } else {
+//                	$scope.firstIndex = $scope.currentPage - 5;
+//                	$scope.lastIndex = $scope.currentPage + 4;
+//                }
+//            }
+//    		$scope.firstIndex = ($scope.currentPage - 1) * $scope.itemsPerPage;
+//    		$scope.lastIndex = $scope.currentPage * $scope.itemsPerPage;
+//    	};
+//    	$scope.updatePageIndexes();
+//    	
+//    	$scope.showList=function(index){
+//    		return ((index >= $scope.firstIndex) && (index < $scope.lastIndex));
+//    	}
     	$scope.getImage = function(element) {
     		photofile = element.files[0];
             var reader = new FileReader();
