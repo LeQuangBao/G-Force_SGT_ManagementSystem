8a1d9bbfc750615ec45e0767d0c46043a840349a
ThachLinh <nlinh6894@gmail.com>
2017-05-23 21:53:04 +0700

    thêm xoa session
    sửa chưa dc do chưa lấy dữ liệu lên dc

--- ProjectISC_Springboot_New/src/main/java/com/isc/dao/SessionDaoImpl.java
+++ ProjectISC_Springboot_New/src/main/java/com/isc/dao/SessionDaoImpl.java
@@ -15,23 +15,23 @@ public class SessionDaoImpl implements SessionDao {
 	private SessionFactory session;
 
 	@SuppressWarnings("unchecked")
-	
+	@Override
 	public List<Session> getAllSessions() {
 		return session.getCurrentSession().createQuery("from Session").list();
 	}
-
+	@Override
 	public Session getSession(int id) {
-		return session.getCurrentSession().load(Session.class, id);
+		return (Session) session.getCurrentSession().load(Session.class, id);
 	}
-
+	@Override
 	public void addSession(Session Session) {
 		session.getCurrentSession().save(Session);
 	}
-
+	@Override
 	public void deleteSession(int id) {
 		session.getCurrentSession().delete(getSession(id));
 	}
-
+	@Override
 	public void updateSession(Session Session) {
 		session.getCurrentSession().update(Session);
 	}
